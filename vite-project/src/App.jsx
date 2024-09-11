3395bd85919c789239f3734d716f62ce8956cb37
Patoliya Henish <henishpatoliya1508@gmail.com>
2024-09-11 18:08:33 +0530

    setup

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Temp from "./simple.jsx";
+
+
+function App() {
+  return (
+    <>
+    <Temp></Temp>
+    <p>this is second tag</p>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/simple.jsx
@@ -0,0 +1,7 @@
+function Temp() {
+    return(
+        <h1>Hello</h1>
+    )
+}
+
+export default Temp;
