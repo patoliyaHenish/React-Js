d5a9015e2597b369fe675939975a61121b15f8ec
Patoliya Henish <henishpatoliya1508@gmail.com>
2024-09-14 20:25:57 +0530

    counter using hooks

---
+++ counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  //hooks to make counter
+  let [counter, setCounter] = useState(5)
+
+  // let counter = 5
+
+  const addValue = () => {
+    if(counter < 20) {
+      // console.log("clicked", counter);
+      counter = counter + 1;    
+      setCounter(counter);
+    }
+  }
+
+  const removeValue= () => {
+    if(counter != 0){
+      counter = counter - 1;
+      setCounter(counter);
+    }
+  }
+
+  return (
+    <>
+      <h1>Henish</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Increase value</button>
+      <br/>
+      <button onClick={removeValue}>Decrease value</button>
+    </>
+  )
+}
+
+export default App
