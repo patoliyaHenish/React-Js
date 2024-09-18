3a3bb258478b9d36a172892dd82fff7c39650ecd
Patoliya Henish <henishpatoliya1508@gmail.com>
2024-09-18 16:39:41 +0530

    password generator using useEffect, useRef, useCallback

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useCallback } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+-=[]{}|;:',.<>?/`~";
+
+    array.forEach((element) => {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    });
+    setPassword(pass);
+  }, [length, numAllowed, charAllowed, setPassword]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-white text-center text-lg my-3">Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="range" min={6} max={100} value={length} className="cursor-pointer" onChange={(e) => {setLength(e.target.value)}}/><label>Length: {length}</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
