bd483e8cd5ff1a56cda95f5ad99628b7713abda6
Patoliya Henish <henishpatoliya1508@gmail.com>
2024-09-20 13:52:56 +0530

    Add Layout component for React Router

---
+++ reactRouter/src/components/GitHub/Github.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+const Github = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/patoliyaHenish')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github followers: {data.followers}
+    </div>
+  );
+};
+
+export default Github;
