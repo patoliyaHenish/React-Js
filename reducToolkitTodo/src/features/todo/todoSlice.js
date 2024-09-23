fc1569610115e4df695b62f0b3864293ca87e616
Patoliya Henish <henishpatoliya1508@gmail.com>
2024-09-23 22:45:17 +0530

    Add Redux Toolkit store configuration and todoSlice

---
+++ reducToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,19 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{id: 1, text:"Hello world"}]
+}
+
+function sayHello() {
+    console.log("Hello world");
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: () => {},
+        removeTodo: () => {},
+        
+    }
+})
