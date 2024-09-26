8c99f27051c008772c2ab24fef40bb245cfb715f
Patoliya Henish <henishpatoliya1508@gmail.com>
2024-09-26 23:05:16 +0530

    Refactor authSlice.js to handle login and logout actions

--- MegaBlog/src/store/authSlice.js
+++ MegaBlog/src/store/authSlice.js
@@ -9,8 +9,17 @@ const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-        login: (state, action) => {}
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
     }
 });
+
+export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;
