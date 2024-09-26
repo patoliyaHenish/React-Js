09d2f43c85f87bb5fd5cd47e0eb2a18dad58dfd8
Patoliya Henish <henishpatoliya1508@gmail.com>
2024-09-26 23:39:26 +0530

    Refactor postcss.config.js and tailwind.config.js

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,13 +1,28 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice"
+import "./App.css";
 
 function App() {
-  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))  
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen"></div>
+  ) : null
 }
 
-export default App
+export default App;
--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -61,4 +61,4 @@ export class AuthService {
 
 const authService = new AuthService();
 
-export default AuthService;
+export default authService;
